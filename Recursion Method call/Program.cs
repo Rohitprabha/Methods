604c6c89d65fb42c9065fef1dbe6f0a94282375c
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-17 21:18:10 +0530

    Add files via upload

---
+++ Recursion Method call/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Recursion_Method
+{
+    class Program
+    {
+        public static int Factorial(int number)
+        {
+            int result;
+            if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                result = number * Factorial(number - 1);
+                return result;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int number;
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Factorial(number));
+        }
+    }
+}
