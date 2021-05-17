9088218ba61b2a37b28d46473ffb3c192d8e7f07
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-17 21:16:05 +0530

    Add files via upload

---
+++ Optional parameters/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Optional_Parameters
+{
+    class Program
+    {
+        public static void Add(int x, int y = 1, int z = 2)
+        {
+            Console.WriteLine(x + y + z);
+        }
+        static void Main(string[] args)
+        {
+            Add(5);
+            Add(5,z:5);
+        }
+    }
+}
