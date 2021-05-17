b16d3eab7d2dff00ea6fcfc037221a8045693933
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-17 21:09:38 +0530

    Add files via upload

---
+++ Ref and out Keyword/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ref_Keyword_and_Out_Keyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void ExtraCredit(ref int studentGrade)
+            {
+                studentGrade += 3;
+                Console.WriteLine("Student grade inside function {0}", studentGrade);
+            }
+            int studentGrade = 75;
+            Console.WriteLine("Student grade before extra credit {0}", studentGrade);
+            ExtraCredit(ref studentGrade);          //pass by value
+            Console.WriteLine("Student grade after extra credit {0}", studentGrade);
+
+
+
+            int add;
+            int mult;
+            AddandMul(5, 10, out add, out mult);
+            Console.WriteLine(add);
+            Console.WriteLine(mult);
+            static void AddandMul(int a, int b, out int added, out int mult)
+            {
+                added = a + b;
+                mult = a * b;
+            }
+        }
+    }
+}
