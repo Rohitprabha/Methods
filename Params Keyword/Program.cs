2f340481909a9716e73f9b20adc4842f11ac9cd8
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-17 21:12:00 +0530

    Add files via upload

---
+++ Params Keyword/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Params_keyword
+{
+    class Program
+    {
+        static void PrintArray(params int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = { 5, 6, 7, 8, 9, 10 };
+            PrintArray(myArray);
+            PrintArray(5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);          //params keyword allows you to pass infinite no.
+                                                                        //of arguments
+
+        }
+    }
+}
